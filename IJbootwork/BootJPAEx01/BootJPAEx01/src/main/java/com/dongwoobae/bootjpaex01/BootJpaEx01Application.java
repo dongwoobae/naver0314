c66f0e57e2171246899726ded147a9308693cb90
dongwoobae <https://github.com/dongwoobae/naver0314devops11.git>
2024-07-03 10:54:28 +0900

    05

---
+++ IJbootwork/BootJPAEx01/BootJPAEx01/src/main/java/com/dongwoobae/bootjpaex01/BootJpaEx01Application.java
@@ -0,0 +1,13 @@
+package com.dongwoobae.bootjpaex01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaEx01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootJpaEx01Application.class, args);
+    }
+
+}
---
+++ IJbootwork/BootJPAEx01/BootJPAEx01/src/test/java/com/dongwoobae/bootjpaex01/BootJpaEx01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongwoobae.bootjpaex01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJpaEx01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
